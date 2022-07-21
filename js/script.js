db2c67934bcb3bc9b3952131df4f74eb3afda67b
stefaniavillella <99300919+stefaniavillella@users.noreply.github.com>
2022-07-20 22:55:06 -0300

    arreglo fetch

--- js/script.js
+++ js/script.js
@@ -2,38 +2,33 @@
 
 // DEFINO ARRAYS Y OBJETOS
 const carrito = JSON.parse(localStorage.getItem("totalCarrito")) || [];
-let productos;
+let productos = "";
 
 
-const traerDatos = () => {
+function traerDatos() {
     fetch('productos.json')
-    .then((response) => response.json())
-    .then((data) => {
-        productos = data.results;
-        console.log(productos);
-        generarCards(productos);
+    .then(function(response){
+        return response.json()
     })
-}
+    .then(function(data){
+        let html = '';
+        data.forEach(function(producto){
+            html += `<div class="col-md-4 mb-3 cards__cardKit">
+            <div class="card producto">
+                <img src="${producto.foto}" class="card-img-top" alt="pascuas">
+                <div class="card-body">
+                <h5 class="card-title cards__cardTitle">${producto.nombre}</h5>
+                <p class="card-text cards__cardText">$${producto.precio}</p>
+                <a href="#" onclick="agregarAlCarrito(${producto.id})" class="btn cards__botonCartas" onclick="agregarAlCarrito()" >Agregar a carrito</a>
+                </div>
+                </div>
+            </div>`
+        });
+        document.getElementById('card-container').innerHTML = html;
+        })
+    }
 
-
-const generarCards = (arrayProductos) => {
-    let cards = ``;
-    arrayProductos.forEach((producto) => {
-        cards += `<div class="col-md-4 mb-3 cards__cardKit">
-        <div class="card producto">
-            <img src="${producto.foto}" class="card-img-top" alt="pascuas">
-            <div class="card-body">
-            <h5 class="card-title cards__cardTitle">${producto.nombre}</h5>
-            <p class="card-text cards__cardText">$${producto.precio}</p>
-            <a href="#" onclick="agregarAlCarrito(${producto.id})" class="btn cards__botonCartas" onclick="agregarAlCarrito()" >Agregar a carrito</a>
-            </div>
-            </div>
-        </div>`
-    });
-    document.getElementById('card-container').innerHTML = cards;
-}
-
-traerDatos();
+    traerDatos();
 
 
 
@@ -58,22 +53,22 @@ chequearBD('').then((productos) => {
     const arrayBD = productos;
     console.log(arrayBD);
 })
-
 */
 
 
 
 
+/*
 
 function filtrarPorCategoria(categoria){
     const listaFiltrada = productos.filter((producto) => producto.categoria == categoria);
     generarCards(listaFiltrada)
 }
-/*
+
 function generarCards(arrayRecorrer){
-    let cards = ``;
+    let html = ``;
 arrayRecorrer.forEach((producto) => {
-    cards += `<div class="col-md-4 mb-3 cards__cardKit">
+    html += `<div class="col-md-4 mb-3 cards__cardKit">
     <div class="card producto">
         <img src="${producto.foto}" class="card-img-top" alt="pascuas">
         <div class="card-body">
@@ -85,9 +80,9 @@ arrayRecorrer.forEach((producto) => {
     </div>`
 });
 
-document.getElementById('card-container').innerHTML = cards;
+document.getElementById('card-container').innerHTML = html;
 }
-*/
+
 
 const carritoStorage = localStorage.getItem('cart');
 const cartParse = JSON.parse(carritoStorage) ?? [];
@@ -103,7 +98,7 @@ function agregarAlCarrito(id){
     <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
     </svg>`+ " $" + totalCarrito;
 }
-
+*/
 
 
 // EVENTO PARA ABRIR POPUP DE CARRITO
